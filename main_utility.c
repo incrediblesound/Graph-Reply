35c019799f30fceb1f4164d14391e987b9680031
James Edwards <jamese@syapse.com>
2015-05-30 23:43:54 -0700

    simplify get_line function

--- main_utility.c
+++ main_utility.c
@@ -4,33 +4,15 @@
 #include <stdlib.h> /* for malloc(3), realloc(3), and free(3) */
 
 char *get_line() {
-    char * line = malloc(100), * linep = line;
-    size_t lenmax = 100, len = lenmax;
-    int c;
-
-    if(line == NULL)
-        return NULL;
-
-    for(;;) {
-        c = fgetc(stdin);
-        if(c == EOF)
+    char *line = malloc(300);
+    int i = 0;
+    while(i == 0 || line[i-1] != '\n'){
+        if(i > 298){
+            printf("{ error: \"Input truncated, reached maximum length.\" }\n");
             break;
-
-        if(--len == 0) {
-            len = lenmax;
-            char * linen = realloc(linep, lenmax *= 2);
-
-            if(linen == NULL) {
-                free(linep);
-                return NULL;
-            }
-            line = linen + (line - linep);
-            linep = linen;
         }
-
-        if((*line++ = c) == '\n')
-            break;
+        line[i] = getchar();
+        i++;
     }
-    *line = '\0';
-    return linep;
+    return line;
 }
